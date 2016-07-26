d536e2cf734b7aa08083b6979b6ca42a48721b23
Mark <mark.c.will@gmail.com>
2016-07-26 14:36:34 -0700

    Version bump bugfix 0.5.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 s_args = {
     'name': 'qmlutil',
-    'version': '0.5.0',
+    'version': '0.5.1',
     'description': 'QuakeML utils for python',
     'author': 'Mark Williams',
     'maintainer': 'Nevada Seismological Laboratory',
