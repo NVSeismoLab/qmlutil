8ae4e274429b5ae1d7ea7a91395f5180fce1bb2f
Mark Williams <mark.c.will@gmail.com>
2020-03-23 12:58:47 -0700

    doc: Version bump to 0.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 s_args = {
     'name': 'qmlutil',
-    'version': '0.9.0',
+    'version': '0.10.0',
     'description': 'QuakeML utils for python',
     'author': 'Mark Williams',
     'maintainer': 'Nevada Seismological Laboratory',
